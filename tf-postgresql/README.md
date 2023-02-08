e89be7b5a604016f3ff1264f74eaf0fbafcb2107
Joven Frankie A. Bico <Joven@FrankieBico.com>
2023-02-08 01:32:02 +0000

    add public revoke db and schema

--- tf-postgresql/README.md
+++ tf-postgresql/README.md
@@ -8,7 +8,11 @@ docker pull postgres:14.6-alpine
 docker run --name my-postgres -e POSTGRES_PASSWORD=t0p-s3cr3t -p 5432:5432 -d postgres:14.6-alpine
 ```
 
-#### Get container IP address
+localhost:5432  
+user `postgres`  
+pass `t0p-s3cr3t`  
+
+#### HOWTO: Get container IP address
 ```
 docker container inspect -f '{{ .NetworkSettings.IPAddress }}' my-postgres
 # 172.18.0.2
--- tf-postgresql/postgres.tf
+++ tf-postgresql/postgres.tf
@@ -2,28 +2,53 @@ locals {
   databases = yamldecode(file("value.yaml"))["databases"]
 }
 
-resource "random_password" "password" {
+# resource "random_password" "password" {
+#   for_each = local.databases
+#   length   = 16
+#   special  = true
+# }
+
+resource "postgresql_role" "role" {
   for_each = local.databases
-  length   = 16
-  special  = true
+
+  name  = each.value.owner
+  login = true
+  # password = random_password.password[each.key].result
+  password = each.value.password
 }
 
-resource "postgresql_role" "my_role" {
-  for_each = local.databases
-
-  name     = each.value.owner
-  login    = true
-  password = random_password.password[each.key].result
-  #   password = "r@nd0m-s3cr3t"
-}
-
-resource "postgresql_database" "my_db" {
+resource "postgresql_database" "db" {
   for_each = local.databases
 
   name  = each.key
   owner = each.value.owner
 
   depends_on = [
-    postgresql_role.my_role
+    postgresql_role.role
   ]
+}
+
+# Revoke default accesses for PUBLIC role to the databases
+resource "postgresql_grant" "public_revoke_database" {
+  for_each = local.databases
+
+  database    = each.key
+  role        = "public"
+  object_type = "database"
+  privileges  = []
+
+  with_grant_option = true
+}
+
+# Revoke default accesses for PUBLIC role to the public schema
+resource "postgresql_grant" "public_revoke_schema" {
+  for_each = local.databases
+
+  database    = each.key
+  role        = "public"
+  schema      = "public"
+  object_type = "schema"
+  privileges  = []
+
+  with_grant_option = true
 }
